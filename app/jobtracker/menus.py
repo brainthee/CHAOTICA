ad42bb8829e442765cef8f4e66d8869f53b3bc44
Adrian Lewis <brainthee@gmail.com>
2023-11-08 14:10:57 +0000

    fix menu issue, avatar 404 and req field on profile

--- app/jobtracker/menus.py
+++ app/jobtracker/menus.py
@@ -1,60 +1,62 @@
 from django.urls import reverse
 from menu import Menu, MenuItem
+from chaotica_utils.utils import PermMenuItem
 
 
-Menu.add_item("main", MenuItem("Jobs",
+Menu.add_item("main", PermMenuItem("Jobs",
                             reverse("job_list"),
                             icon="cubes",
-                            check=lambda request: request.user.has_perm('jobtracker.view_job'),
+                            perm='jobtracker.view_job',
                             weight=1,))
 
-Menu.add_item("main", MenuItem("Scheduler",
+Menu.add_item("main", PermMenuItem("Scheduler",
                             reverse("view_scheduler"),
                             icon="calendar",
-                            check=lambda request: request.user.has_perm('jobtracker.view_scheduler'),
+                            perm='jobtracker.view_scheduler',
                             weight=2,))
-Menu.add_item("main", MenuItem("Clients",
+
+Menu.add_item("main", PermMenuItem("Clients",
                             reverse("client_list"),
                             icon="handshake",
-                            check=lambda request: request.user.has_perm('jobtracker.view_client'),
+                            perm='jobtracker.view_client',
                             weight=3,))
 
 
-Menu.add_item("main", MenuItem("Reporting",
+Menu.add_item("main", PermMenuItem("Reporting",
                             reverse("view_reports"),
                             weight=4,
-                            check=lambda request: request.user.has_perm('jobtracker.view_report'),
+                            perm='jobtracker.view_report',
                             icon="file-lines"))
 
 ops_children = (
     MenuItem("Skills",
                 reverse("skill_list"),
                 icon="readme",
-                check=lambda request: request.user.has_perm('jobtracker.view_skill'),
+                perm='jobtracker.view_skill',
                 icon_prefix="fab fa-",
                 weight=1,
                 ),
     MenuItem("Services",
                 reverse("service_list"),
-                check=lambda request: request.user.has_perm('jobtracker.view_service'),
+                perm='jobtracker.view_service',
                 icon="laptop-code",
                 weight=1,
                 ),
     MenuItem("Workflow Checklists",
                 reverse("wf_tasks_list"),
-                check=lambda request: request.user.has_perm('jobtracker.view_workflowtask'),
+                perm='jobtracker.view_workflowtask',
                 icon="list-check",
                 weight=1,
                 ),
     MenuItem("Manage Leave",
                 reverse("manage_leave"),
                 icon="person-walking-arrow-right",
-                check=lambda request: request.user.has_perm('jobtracker.manage_leave'),
+                perm='jobtracker.manage_leave',
                 weight=1,
                 ),
     MenuItem("Organisational Units",
                 reverse("organisationalunit_list"),
-                check=lambda request: request.user.has_perm('jobtracker.view_organisationalunit'),
+                perm='jobtracker.view_organisationalunit',
                 icon="building",
                 weight=10,),
 )
