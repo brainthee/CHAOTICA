592c1695c09362da55b9d30dbf24760de7123b1f
Adrian Lewis <brainthee@gmail.com>
2023-11-09 16:39:18 +0000

    This might be my favourite thing ever....

---
+++ app/static/assets/js/chaotica.js
@@ -0,0 +1,12 @@
+function onKonamiCode(cb) {
+    var input = '';
+    var key = '38384040373937396665';
+    document.addEventListener('keydown', function (e) {
+      input += ("" + e.keyCode);
+      if (input === key) {
+        return cb();
+      }
+      if (!key.indexOf(input)) return;
+      input = ("" + e.keyCode);
+    });
+}
