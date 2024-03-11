d4768711aef0df3bcc12f9c547b43d325920fa29
Adrian Lewis <brainthee@gmail.com>
2024-03-11 12:03:56 +0000

    Start cleaning up perms

--- app/chaotica_utils/enums.py
+++ app/chaotica_utils/enums.py
@@ -87,7 +87,7 @@ class GlobalRoles():
             "jobtracker.view_client", "jobtracker.add_client", "jobtracker.change_client", 
             "jobtracker.delete_client", "jobtracker.assign_account_managers_client",
             # Service
-            "jobtracker.view_service", 'jobtracker.assign_to_phase',
+            "jobtracker.view_service",
             # SkillCategory
             "jobtracker.view_skillcategory",
             # Skill
@@ -110,7 +110,7 @@ class GlobalRoles():
             # OrganisationalUnit
             "jobtracker.view_organisationalunit",
             # Certification
-            "jobtracker.view_certification",
+            "jobtracker.view_certification", 'jobtracker.view_users_certification',
         ]),
         (SALES_MEMBER, [ 
             # Client
@@ -135,11 +135,11 @@ class GlobalRoles():
             # SkillCategory
             "jobtracker.view_skillcategory",
             # Skill
-            "jobtracker.view_skill",
+            "jobtracker.view_skill", 'jobtracker.view_users_skill',
             # OrganisationalUnit
             "jobtracker.view_organisationalunit",
             # Certification
-            "jobtracker.view_certification",
+            "jobtracker.view_certification", 'jobtracker.view_users_certification',
         ]),
     )
 
@@ -200,6 +200,7 @@ class UnitRoles():
         ]),
         (MANAGER, [
             # OrganisationalUnit
+            "jobtracker.can_view_unit_jobs",
             "jobtracker.change_organisationalunit", "jobtracker.delete_organisationalunit",
             "jobtracker.manage_members", 'jobtracker.view_users_schedule',
             "jobtracker.can_scope_jobs", 'jobtracker.can_signoff_scopes',
