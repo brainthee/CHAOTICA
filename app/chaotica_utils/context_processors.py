ab394a7297be05babeb6a366a4e6121990484cec
Adrian Lewis <brainthee@gmail.com>
2024-04-03 17:26:40 +0100

    cleanup some early DB requests

--- app/chaotica_utils/context_processors.py
+++ app/chaotica_utils/context_processors.py
@@ -3,14 +3,4 @@ from .models import User
 from jobtracker.models import OrganisationalUnit, Client, Service
 
 def defaults(_):
-    new_install = User.objects.all().count() <= 1
-    setup_unit = OrganisationalUnit.objects.all().count() == 0
-    setup_client = Client.objects.all().count() == 0
-    setup_service = Service.objects.all().count() == 0
-
-    return {
-        'new_install': new_install,
-        'setup_unit': setup_unit,
-        'setup_client': setup_client,
-        'setup_service': setup_service,
-    }
+    return {}
