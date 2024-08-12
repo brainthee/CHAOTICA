ffc6cfe70d3444a062ace8a05b39ac8c282969c0
Adrian Lewis <brainthee@gmail.com>
2024-08-12 14:07:17 +0100

    Disable ACLs

--- app/chaotica/settings.py
+++ app/chaotica/settings.py
@@ -441,7 +441,7 @@ if USE_S3 == "1" or USE_S3:
     AWS_ACCESS_KEY_ID = os.getenv("AWS_STORAGE_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_STORAGE_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read"
+    # AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     STATICFILES_LOCATION = 'static'  # staticfiles will be in 'static'
     AWS_S3_SIGNATURE_VERSION = 's3v4'
