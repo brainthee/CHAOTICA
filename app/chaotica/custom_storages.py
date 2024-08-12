3181d1d5c38ff49dac7a30dd65445d856886ca81
Adrian Lewis <brainthee@gmail.com>
2024-08-12 11:32:26 +0100

    More CF changes

--- app/chaotica/custom_storages.py
+++ app/chaotica/custom_storages.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
@@ -9,9 +8,9 @@ class StaticStorage(S3Boto3Storage):
         kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
         super(StaticStorage, self).__init__(*args, **kwargs)
 
-class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+# class MediaStorage(S3Boto3Storage):
+#     location = settings.MEDIAFILES_LOCATION
 
-    def __init__(self, *args, **kwargs):
-        kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
-        super(MediaStorage, self).__init__(*args, **kwargs)
+#     def __init__(self, *args, **kwargs):
+#         kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
+#         super(MediaStorage, self).__init__(*args, **kwargs)
