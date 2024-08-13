02f85a0e65a90a41f5b8567dfd357c5f74230c04
Adrian Lewis <brainthee@gmail.com>
2024-08-13 13:08:57 +0100

    Enable media stuff

--- app/chaotica/custom_storages.py
+++ app/chaotica/custom_storages.py
@@ -8,9 +8,9 @@ class StaticStorage(S3Boto3Storage):
         kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
         super(StaticStorage, self).__init__(*args, **kwargs)
 
-# class MediaStorage(S3Boto3Storage):
-#     location = settings.MEDIAFILES_LOCATION
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
 
-#     def __init__(self, *args, **kwargs):
-#         kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
-#         super(MediaStorage, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
+        super(MediaStorage, self).__init__(*args, **kwargs)
