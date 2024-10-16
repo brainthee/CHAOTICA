09c59a464215267c6c185a51bc4337529c8a52ee
Adrian Lewis <brainthee@gmail.com>
2024-10-16 17:04:01 +0100

    Improve performance of dashboard

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -4,12 +4,13 @@ from django.contrib.auth.decorators import login_required
 import logging
 from django.utils import timezone
 from datetime import datetime, timedelta
-from jobtracker.models import Job, Phase, TimeSlot
+from jobtracker.models import Job, Phase, TimeSlot, OrganisationalUnit
 from chaotica_utils.models import LeaveRequest
 from jobtracker.enums import JobStatuses, PhaseStatuses
 from chaotica_utils.views import page_defaults
 from django.db.models import Q
 from django.views.decorators.http import require_safe
+from guardian.shortcuts import get_objects_for_user
 
 logger = logging.getLogger(__name__)
 
@@ -30,12 +31,25 @@ def index(request):
     context = {}
     template = loader.get_template("dashboard_index.html")
 
-    context["in_flight"] = (
-        Phase.objects.phases_with_unit_permission(request.user, "can_view_jobs")
-        .filter(Q(status=PhaseStatuses.IN_PROGRESS))
-        .order_by("desired_delivery_date")
-    )
+    view_units = get_objects_for_user(request.user, "can_view_jobs", klass=OrganisationalUnit)
+    all_phases = Phase.objects.filter(
+            Q(job__unit__in=view_units),
+            status__in=PhaseStatuses.ACTIVE_STATUSES,  # Include active phase statuses only
+            job__status__in=JobStatuses.ACTIVE_STATUSES,  # Include active job statuses only
+        ).prefetch_related()
 
+    context["in_flight"] = all_phases.filter(Q(status=PhaseStatuses.IN_PROGRESS))
+    context["TQA"] = all_phases.filter(
+            Q(status=PhaseStatuses.PENDING_TQA)
+            | Q(status=PhaseStatuses.QA_TECH)
+            | Q(status=PhaseStatuses.QA_TECH_AUTHOR_UPDATES)
+        )
+    context["PQA"] = all_phases.filter(
+            Q(status=PhaseStatuses.PENDING_PQA)
+            | Q(status=PhaseStatuses.QA_PRES)
+            | Q(status=PhaseStatuses.QA_PRES_AUTHOR_UPDATES)
+        )
+    
     # this week's timeslots
     week_start_date = timezone.datetime.today() - timedelta(
         days=timezone.datetime.today().weekday()
@@ -48,40 +62,21 @@ def index(request):
         .values_list("phase", flat=True)
         .distinct()
     )
-    context["scheduled_phases_this_week"] = Phase.objects.phases_with_unit_permission(
-        request.user, "can_view_jobs"
-    ).filter(pk__in=phases_this_week)
+    context["scheduled_phases_this_week"] = all_phases.filter(pk__in=phases_this_week)
 
-    context["pendingScoping"] = Job.objects.jobs_with_unit_permission(
-        request.user, "can_scope_jobs"
-    ).filter(
+
+    scope_units = get_objects_for_user(request.user, "can_scope_jobs", klass=OrganisationalUnit)
+    signoff_units = get_objects_for_user(request.user, "can_signoff_scopes", klass=OrganisationalUnit)
+
+    context["pendingScoping"] = Job.objects.filter(
+        Q(unit__in=scope_units),
         Q(status=JobStatuses.PENDING_SCOPE)
         | Q(status=JobStatuses.SCOPING_ADDITIONAL_INFO_REQUIRED)
     )
 
-    context["scopesToSignoff"] = Job.objects.jobs_with_unit_permission(
-        request.user, "can_signoff_scopes"
-    ).filter(status=JobStatuses.PENDING_SCOPING_SIGNOFF)
-
-    context["TQA"] = (
-        Phase.objects.phases_with_unit_permission(request.user, "can_tqa_jobs")
-        .filter(
-            Q(status=PhaseStatuses.PENDING_TQA)
-            | Q(status=PhaseStatuses.QA_TECH)
-            | Q(status=PhaseStatuses.QA_TECH_AUTHOR_UPDATES)
-        )
-        .order_by("desired_delivery_date")
-    )
-
-    context["PQA"] = (
-        Phase.objects.phases_with_unit_permission(request.user, "can_pqa_jobs")
-        .filter(
-            Q(status=PhaseStatuses.PENDING_PQA)
-            | Q(status=PhaseStatuses.QA_PRES)
-            | Q(status=PhaseStatuses.QA_PRES_AUTHOR_UPDATES)
-        )
-        .order_by("desired_delivery_date")
-    )
+    context["scopesToSignoff"] = Job.objects.filter(
+        Q(unit__in=signoff_units),
+        status=JobStatuses.PENDING_SCOPING_SIGNOFF)
 
     if request.user.is_people_manager():
         context["leaveToReview"] = LeaveRequest.objects.filter(
