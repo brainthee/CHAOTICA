fff87bd8650a84fd8e6ce2ee7dc5e1c0aa79a7df
Adrian <brainthee@gmail.com>
2025-02-11 16:30:39 +0000

    Disable memcache by default

--- app/chaotica/settings.py
+++ app/chaotica/settings.py
@@ -512,13 +512,13 @@ else:
     }
 
 
-CACHES = {
-    'default': {
-        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
-CONSTANCE_DATABASE_CACHE_BACKEND = 'default'
+# CACHES = {
+#     'default': {
+#         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+#         'LOCATION': '127.0.0.1:11211',
+#     }
+# }
+# CONSTANCE_DATABASE_CACHE_BACKEND = 'default'
 
 
 
