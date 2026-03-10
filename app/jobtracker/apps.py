a532d7d96d5a522df19f9818dcceded9647f00b3
Adrian <brainthee@gmail.com>
2026-03-10 13:53:06 +0000

    Fix incorrect use of apps running DB queries.

--- app/jobtracker/apps.py
+++ app/jobtracker/apps.py
@@ -1,69 +1,57 @@
 from django.apps import AppConfig
-from django.db import connections
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def table_exists(table_name: str) -> bool:
-    return table_name in connections["default"].introspection.table_names()
-
-
-def populate_timeslot_types():
+def populate_timeslot_types(sender, **kwargs):
     from .models import TimeSlotType
     from .enums import DefaultTimeSlotTypes
 
-    if table_exists("jobtracker_timeslottype"):
-        if (
-            not TimeSlotType.objects.all().count()
-        ):  # Don't run if we already have types in the DB
-            for default_type in DefaultTimeSlotTypes.DEFAULTS:
-                instance, created = TimeSlotType.objects.get_or_create(
-                    pk=default_type["pk"], defaults=default_type
-                )
-                if created:
-                    for attr, value in default_type.items():
-                        setattr(instance, attr, value)
-                    instance.save()
+    if not TimeSlotType.objects.all().count():
+        # Don't run if we already have types in the DB
+        for default_type in DefaultTimeSlotTypes.DEFAULTS:
+            instance, created = TimeSlotType.objects.get_or_create(
+                pk=default_type["pk"], defaults=default_type
+            )
+            if created:
+                for attr, value in default_type.items():
+                    setattr(instance, attr, value)
+                instance.save()
 
 
-def populate_default_unit_roles():
+def populate_default_unit_roles(sender, **kwargs):
     from .models import OrganisationalUnitRole
     from chaotica_utils.enums import UnitRoles
     from django.contrib.auth.models import Permission
 
-    if (
-        table_exists("jobtracker_organisationalunitrole")
-        and Permission.objects.filter(codename="view_client").exists()
-    ):  # check DB is intact:
-        if (
-            not OrganisationalUnitRole.objects.all().count()
-        ):  # Don't run if we already have roles in the DB
-            for role in UnitRoles.DEFAULTS:
-                if role["pk"] == 0:
-                    continue
-                instance, created = OrganisationalUnitRole.objects.get_or_create(
-                    pk=role["pk"], name=role["name"]
-                )
-                if created:
-                    for attr, value in role.items():
-                        setattr(instance, attr, value)
+    if not OrganisationalUnitRole.objects.all().count():
+        # Don't run if we already have roles in the DB
+        for role in UnitRoles.DEFAULTS:
+            if role["pk"] == 0:
+                continue
+            instance, created = OrganisationalUnitRole.objects.get_or_create(
+                pk=role["pk"], name=role["name"]
+            )
+            if created:
+                for attr, value in role.items():
+                    setattr(instance, attr, value)
 
-                    for perm in UnitRoles.PERMISSIONS[role["pk"]-1][1]:
-                        if perm:
-                            codeword = perm
-                            if "." in perm:
-                                codeword = perm.split(".")[1]
-                            if Permission.objects.filter(codename=codeword).exists():
-                                permission = Permission.objects.get(codename=codeword)
-                                instance.permissions.add(permission)
-                            else:
-                                logger.error(
-                                    "ERROR: Unknown Permission - {full}".format(
-                                        full=perm
-                                    )
+                for perm in UnitRoles.PERMISSIONS[role["pk"]-1][1]:
+                    if perm:
+                        codeword = perm
+                        if "." in perm:
+                            codeword = perm.split(".")[1]
+                        if Permission.objects.filter(codename=codeword).exists():
+                            permission = Permission.objects.get(codename=codeword)
+                            instance.permissions.add(permission)
+                        else:
+                            logger.error(
+                                "ERROR: Unknown Permission - {full}".format(
+                                    full=perm
                                 )
-                    instance.save()
+                            )
+                instance.save()
 
 
 class JobtrackerConfig(AppConfig):
@@ -71,8 +59,10 @@ class JobtrackerConfig(AppConfig):
     name = "jobtracker"
 
     def ready(self):
-        populate_default_unit_roles()
-        populate_timeslot_types()
+        from django.db.models.signals import post_migrate
+
+        post_migrate.connect(populate_default_unit_roles, sender=self)
+        post_migrate.connect(populate_timeslot_types, sender=self)
 
         # Import signal handlers
         from . import signals  # noqa: F401
